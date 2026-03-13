760c4034ccc1c4c23bc24287e48135612e32444b
Brian Nunez <bnunez9999@gmail.com>
2026-03-12 21:21:12 -0700

    feat(nvim): update load paths

--- javascript.sh
+++ javascript.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[ -d "$HOME/nvim-macos/bin" ] && export PATH="/Users/briannunez/nvim-macos/bin/:$PATH"
-[ -d "$HOME/nvim-linux-x86_64/bin" ] && export PATH="$HOME/nvim-linux-x86_64/bin:$PATH"
+[ -d "$HOME/nvim-macos/bin" ] && export PATH="$HOME/nvim-macos/bin:$PATH"
+[ -d "/opt/nvim/bin" ] && export PATH="/opt/nvim/bin:$PATH"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
